b191846b851b27a788cb917d49db659e05883965
Michel Oliveira <micheltdoliveira@gmail.com>
2022-11-16 23:47:09 -0300

    make Table a component

---
+++ src/components/Table.js
@@ -0,0 +1,26 @@
+import News from "../components/News";
+
+function Table(){
+    return(
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Imagem</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Tema</th>
+                    <th scope="col">Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
+                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
+                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
+            </tbody>
+
+        </table>
+
+    );
+}
+
+export default Table;
--- src/pages/Panel.js
+++ src/pages/Panel.js
@@ -1,25 +1,8 @@
-import News from "../components/News";
+import Table from "../components/Table";
 
 function Panel(){
     return(
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Imagem</th>
-                    <th scope="col">Título</th>
-                    <th scope="col">Descrição</th>
-                    <th scope="col">Tema</th>
-                    <th scope="col">Link</th>
-                </tr>
-            </thead>
-            <tbody>
-                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
-                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
-                <News image="image" title="tilte2" description="jhdjfhsakfja" theme="Musica" link="www.soundcloud.com"/>
-            </tbody>
-
-        </table>
-
+        <Table/>
     );
 }
 
