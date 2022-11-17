7504f31c411c4126b2162c551045e303fc009759
Michel Oliveira <micheltdoliveira@gmail.com>
2022-11-17 01:38:08 -0300

    add header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
+import logo from "../assets/elifeLogoWhite.png"
 
 function Header() {
     return(
-        <header>
-            <h2>header</h2>
+
+        <header class="navbar navbar-dark bg-dark d-flex">
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 " href="#"><img src={logo} height= {32} alt="logo" /></a>
+            <div class="navbar-nav">
+                <div class="nav-item text-nowrap">
+                    <a class="nav-link px-3" href="#">Sign out</a>
+                </div>
+            </div>
         </header>
     );
     
 }
 
 export default Header;
+
--- src/pages/Panel.js
+++ src/pages/Panel.js
@@ -1,8 +1,20 @@
+import Header from "../components/Header";
 import Table from "../components/Table";
 
 function Panel(){
     return(
-        <Table/>
+        <>
+        <main className="col-md-9  col-lg-12">
+            <Header/>
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h4 className="h4 ">Casper Dashboard</h4>
+                <button type="button" class="btn btn-outline-dark">Adicionar Notícia</button>
+
+            </div>
+            <Table/>
+
+        </main>
+        </>
     );
 }
 
