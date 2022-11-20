f5581ed6c957484fbeaf9aee337c1d1a86373e1e
Michel Oliveira <micheltdoliveira@gmail.com>
2022-11-20 20:24:05 -0300

    add basic routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Panel from './pages/Panel';
 
 function App() {
   return (
     <div className="App">
-      <Panel/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Panel/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import logo from "../assets/elifeLogoWhite.png"
 function Header() {
     return(
 
-        <header class="navbar navbar-dark bg-dark d-flex">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 " href="#"><img src={logo} height= {32} alt="logo" /></a>
-            <div class="navbar-nav">
-                <div class="nav-item text-nowrap">
-                    <a class="nav-link px-3" href="#">Sign out</a>
+        <header className="navbar navbar-dark bg-dark d-flex">
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 " href="/"><img src={logo} height= {32} alt="logo" /></a>
+            <div className="navbar-nav">
+                <div className="nav-item text-nowrap">
+                    <a className="nav-link px-3" href="/">Sign out</a>
                 </div>
             </div>
         </header>
