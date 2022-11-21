ca17df60e945f46a6d2ad3d62287e785d0e0613d
Michel Oliveira <micheltdoliveira@gmail.com>
2022-11-20 23:02:39 -0300

    add login with firebase auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/elifeLogo.png"
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../services/Firebase";
+
+
+
 const Login = () =>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = getAuth(app);
+
+
+    function handleSignIn(e){
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                // ...
+                console.log("ok!")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+
     return(
         <div className='container col-sm-6 col-md-6 col-lg-3 mt-5'>
             <main className="form-signin w-100 m-auto">
@@ -9,12 +35,14 @@ const Login = () =>{
                     <h1 className="h3 mb-3 fw-normal">Sign in</h1>
 
                     <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label for="floatingInput">Email address</label>
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                        onChange={e => setEmail(e.target.value)}/>
+                    <label htmlFor="floatingInput">Email address</label>
                     </div>
                     <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                    <label for="floatingPassword">Password</label>
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                        onChange={e => setPassword(e.target.value)}/>
+                    <label htmlFor="floatingPassword">Password</label>
                     </div>
 
                     <div className="checkbox mb-3">
@@ -22,7 +50,7 @@ const Login = () =>{
                         <input type="checkbox" value="remember-me"/> Remember me
                     </label>
                     </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={handleSignIn}>Sign in</button>
                     <p className="mt-5 mb-3 text-muted">© 2022</p>
                 </form>
             </main>
--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBdEpK3PLWlHG0AqeMeL-YiLOooV9bnW0I",
   authDomain: "casper-chatbot-nkfe.firebaseapp.com",
@@ -15,4 +11,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
