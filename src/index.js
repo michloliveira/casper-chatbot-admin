8f88aebbe782ab31099751d43d626a8a8299bdbe
Michel Oliveira <micheltdoliveira@gmail.com>
2022-11-22 20:05:24 -0300

    add create elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/CreateNews.js
+++ src/pages/CreateNews.js
@@ -2,19 +2,63 @@ import { useEffect, useState } from "react";
 import NewsLogo from "../assets/news.jpg";
 
 import { addDoc, collection } from "firebase/firestore";
-import { db } from "../services/Firebase";
+import { db, storage } from "../services/Firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const CreateNews = () =>{
     let imageHeight = {
         height: 175
     }
 
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [link, setLink] = useState("");
+    const [imagem, setImagem] = useState("");
+    const [tema, setTema] = useState("");
+
+
+
+
     const [file, setFile] = useState(null);
 
     useEffect(() => {
         if(file){
 
-            console.log(file.name);
+            const name = new Date().getTime() + file.name
+            const storageRef = ref(storage, name); 
+            console.log(name);
+
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed', 
+            (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                    default:
+                        break;
+                }
+            }, 
+            (error) => {
+                // Handle unsuccessful uploads
+            }, 
+            () => {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                });
+            }
+            );
         }
 
     },[file]);
@@ -36,8 +80,8 @@ const CreateNews = () =>{
             <form onSubmit={handleAdd}>
             <div>
                 <label className="form-label">Imagem</label>
-                <div class="mb-4 d-flex justify-content-center">
-                    {file ? <img src={URL.createObjectURL(file)} style={imageHeight}/> : <img src={NewsLogo} style={imageHeight}/>}
+                <div className="mb-4 d-flex justify-content-center">
+                    {file ? <img src={URL.createObjectURL(file)} style={imageHeight} alt="imagem anuncio"/> : <img src={NewsLogo} style={imageHeight} alt="imagem anuncio"/>}
                 </div>
                 <div className="d-flex justify-content-center">
                     <div className="btn btn-primary btn-rounded btn-sm">
@@ -51,7 +95,14 @@ const CreateNews = () =>{
                     <label className="form-label" >Título</label>
                     <input type="text"  className="form-control" />
                 </div>
-
+                <div className="form-outline mb-4">
+                    <select className="form-select" aria-label="Default select example">
+                        <option selected value= "0" >Entretenimento</option>
+                        <option value="1">Música</option>
+                        <option value="2">Esportes</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
 
                 <div className="form-outline mb-4">
                     <label className="form-label" >Descrição</label>
@@ -60,7 +111,7 @@ const CreateNews = () =>{
 
 
 
-                <button type="submit" className="btn btn-primary btn-block mb-4">Send</button>
+                <button type="submit" className="btn btn-primary btn-block mb-4">Enviar</button>
             </form>
         </main>
         </>
